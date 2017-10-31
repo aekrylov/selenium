3f205253cf2128e5da508ba43b9bd22fd5b01d70
Anton Krylov <anthony.kryloff@gmail.com>
2017-10-31 23:32:23 +0300

    hw02: add common classes, add onemore test, add dtos

---
+++ hw1/src/test/java/com/github/aekrylov/itis/sem05/selenium/Authorization.java
@@ -0,0 +1,16 @@
+package com.github.aekrylov.itis.sem05.selenium;
+
+import org.junit.Test;
+
+/**
+ * By Anton Krylov (anthony.kryloff@gmail.com)
+ * Date: 10/24/17 10:11 PM
+ */
+public class Authorization extends TestBase {
+
+    @Test
+    public void testLogin() {
+        login();
+    }
+
+}
